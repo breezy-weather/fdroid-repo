393bb365c1e89ec6fc3a2cca9e660f7a8a10d4dd
Julian <juliannfairfax@protonmail.com>
2023-08-16 17:37:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Breezy Weather
+The F-Droid repo for Breezy Weather
+
+## How to use
+Copy this link and add it to your F-Droid client:
+
+```
+https://julianfairfax.github.io/fdroid-repo/fdroid/repo?fingerprint=83ABB548CAA6F311CE3591DDCA466B65213FD0541352502702B1908F0C84206D
+```
+
+You can also scan this QR code and add the link to your F-Droid client:
+
+![F-Droid repo QR code](https://julianfairfax.github.io/fdroid-repo/fdroid/repo/icons/icon.png)
---
+++ update.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+declare -a github_repos=("breezy-weather/breezy-weather:Internet")
+
+if [[ ! -d fdroid/metadata ]]; then
+	mkdir -p fdroid/metadata
+fi
+
+if [[ ! -d fdroid/repo ]]; then
+	mkdir -p fdroid/repo
+fi
+
+for github_repo in ${github_repos[@]}; do
+	description=""
+
+	repo=$(echo $github_repo | sed 's|:.*||')
+	wget -q -O latest https://api.github.com/repos/$repo/releases/latest
+	release=$(cat latest | grep tag_name | sed 's/.*tag_name\":\ \"//' | sed 's/\",//')
+	changelog="$(cat latest | sed -z 's/"\n}//g' | grep body | sed 's/  "body": "//' | sed 's/",//' | sed 's/\\r//g' | sed 's/\\n/  \n/g')"
+	urls=$(cat latest | grep browser_download_url | sed 's/      "browser_download_url": "//' | sed 's/"//')
+	url=$(echo "$urls" | grep .apk$ | grep -v debug | grep -v arm64-v8a | grep -v armeabi-v7a | grep -v x86 | grep -v x86_64)
+	asset=$(echo $url | sed 's/.*\///')
+
+	wget -q -O repo https://api.github.com/repos/$repo
+	if [[ ! $(cat repo | grep description -m 1 | sed 's/  "description": "//' | sed 's/",//') == *null* ]]; then
+		description=$(cat repo | grep description -m 1 | sed 's/  "description": "//' | sed 's/",//')
+	fi
+
+	wget -q -O fdroid/repo/$asset $url
+
+	name=$(aapt dump badging fdroid/repo/$asset | grep application-label: | sed "s/application-label:'//" | sed "s/'.*//")
+	version=$(aapt dump badging fdroid/repo/$asset | grep versionCode | sed "s/.*versionCode='//" | sed "s/'.*//")
+	id=$(aapt dump badging fdroid/repo/$asset | grep package:\ name | sed "s/package: name='//" | sed "s/'.*//")
+
+	if [[ -z "$description" ]]; then
+		description="$name"
+	else
+		description="$description"
+	fi
+
+	for anti_feature in ${anti_features[@]}; do
+		if [[ $(echo $anti_feature | sed 's|:.*||') == $repo  ]]; then
+			if [[ ! -f fdroid/metadata/$id.yml ]]; then
+				echo "AntiFeatures:" | tee fdroid/metadata/$id.yml
+			fi
+
+			if [[ ! $(cat fdroid/metadata/$id.yml | grep $anti_feature) == $anti_feature ]]; then
+				echo "    - $(echo $anti_feature | sed 's|.*:||')" | tee -a fdroid/metadata/$id.yml
+			fi
+		fi
+	done
+
+	echo "AuthorName: $(echo $github_repo | sed 's|/.*||')
+Categories:
+    - $(echo $github_repo | sed 's|.*:||')
+CurrentVersion: $release
+CurrentVersionCode: $version
+Description: |
+    $description
+IssueTracker: https://github.com/$repo/issues
+Name: $name
+SourceCode: https://github.com/$repo
+Summary: \"$(echo $description | cut -c 1-80)\"
+WebSite: https://github.com/$repo
+Changelog: https://github.com/$repo/releases" | tee -a fdroid/metadata/$id.yml
+
+	mkdir -p fdroid/metadata/$id/en-US/changelogs
+
+	echo "$changelog" | tee fdroid/metadata/$id/en-US/changelogs/$version.txt
+
+	wget -q -O releases https://api.github.com/repos/$repo/releases
+	changelog="$(cat releases | grep -m1 '"prerelease": true,' -B31 -A224 | sed -z 's/"\n}//g' | grep body | sed 's/  "body": "//' | sed 's/",//' | sed 's/\\r//g' | sed 's/\\n/  \n/g')"
+	urls=$(cat releases | grep -m1 '"prerelease": true,' -B31 -A224 | grep browser_download_url | sed 's/      "browser_download_url": "//' | sed 's/"//')
+	url=$(echo "$urls" | grep .apk$ | grep -v debug | grep -v arm64-v8a | grep -v armeabi-v7a | grep -v x86 | grep -v x86_64)
+	asset=$(echo $url | sed 's/.*\///')
+
+	if [[ $(cat releases | grep -m1 '"prerelease": true,' -B31 -A224 | grep created_at -m1 | sed 's/.* "//' | sed 's/T.*//') -ge $(cat latest | grep created_at -m1 | sed 's/.* "//' | sed 's/T.*//') ]]; then
+		wget -q -O fdroid/repo/$asset $url
+
+		mkdir -p fdroid/metadata/$id/en-US/changelogs
+
+		echo "$changelog" | tee fdroid/metadata/$id/en-US/changelogs/$version.txt
+	fi
+
+	rm latest
+
+	rm releases
+
+	rm repo
+done
+
+if [[ ! -f fdroid/repo/index-v1.json ]]; then
+	echo "{\"repo\": {\"timestamp\": $(date +%s%N | cut -b1-13), \"version\": 20000, \"name\": \"Breezy Weather\", \"icon\": \"icon.png\", \"address\": \"https://julianfairfax.github.io/fdroid-repo/fdroid/repo?fingerprint=83ABB548CAA6F311CE3591DDCA466B65213FD0541352502702B1908F0C84206D\", \"description\": \"The F-Droid repository for Breezy Weather\"}, \"requests\": {\"install\": [], \"uninstall\": []}, \"apps\": [], \"packages\": {}}" | tee fdroid/repo/index-v1.json
+fi
+
+cd fdroid
+
+/usr/bin/fdroid update --pretty --delete-unknown
+
+cd ../

db2abbde93da17136bdd6f6dd5b77082c7b28869
Julian <juliannfairfax@protonmail.com>
2023-09-18 18:42:10 +0200

    Hopefully this works

--- update.sh
+++ update.sh
@@ -22,7 +22,6 @@ for github_repo in ${github_repos[@]}; do
 	asset=$(echo $url | sed 's/.*\///')
 
 	wget -q -O fdroid/repo/$asset $url
-	touch -d $(cat latest | grep published_at | sed 's/.*published_at\":\ \"//' | sed 's/T.*//' | sed 's/-//g') fdroid/repo/$asset
 
 	name=$(aapt dump badging fdroid/repo/$asset | grep application-label: | sed "s/application-label:'//" | sed "s/'.*//")
 	version=$(aapt dump badging fdroid/repo/$asset | grep versionCode | sed "s/.*versionCode='//" | sed "s/'.*//")
@@ -102,7 +101,6 @@ AllowedAPKSigningKeys: 29d435f70aa9aec3c1faff7f7ffa6e15785088d87f06ecfcab9c3cc62
 
 			if [[ $(cat releases | grep -m1 '"prerelease": true,' -B31 -A224 | grep created_at -m1 | sed 's/.* "//' | sed 's/T.*//' | sed 's/-//g') -ge $(cat latest | grep created_at -m1 | sed 's/.* "//' | sed 's/T.*//' | sed 's/-//g') ]]; then
 				wget -q -O fdroid/repo/$asset $url
-				touch -d $(cat releases | grep published_at | sed 's/.*published_at\":\ \"//' | sed 's/T.*//' | sed 's/-//g') fdroid/repo/$asset
 			fi
 
 			rm latest
@@ -149,6 +147,6 @@ done
 
 cd fdroid
 
-/usr/bin/fdroid update --pretty --delete-unknown
+/usr/bin/fdroid update --pretty --delete-unknown --use-date-from-apk
 
 cd ../
